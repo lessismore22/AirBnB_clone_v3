c593ac1eea25d15d041bfb169937640e50ae79fe
lessismore22 <charlesigbe@gmail.com>
2024-05-29 04:01:01 +0000

    amenities.py .py fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-views
-
+This contains the blueprint for our api
 """
 
 from flask import Blueprint
@@ -12,6 +11,7 @@ from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.amenities import *
 from api.v1.views.cities import *
+from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from api.v1.views.users import *
 from api.v1.views.places_amenities import *
